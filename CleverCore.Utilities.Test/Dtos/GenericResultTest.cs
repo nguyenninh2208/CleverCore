a633b385d2a0608f78179b4478b9c1405e164391
NinhNV13 <ninhnv13@fpt.com.vn>
2022-03-16 17:29:39 +0700

    update commit

---
+++ CleverCore.Utilities.Test/Dtos/GenericResultTest.cs
@@ -0,0 +1,32 @@
+﻿using CleverCore.Utilities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleverCore.Utilities.Test.Dtos
+{
+    public class GenericResultTest
+    {
+        [Fact]
+        public void Contructor_CreateObjectNotNull_ValidObjectNoPramDefault()
+        {
+            var genericResult = new GenericResult();
+            Assert.NotNull(genericResult);
+        }
+        [Fact]
+        public void Contructor_CreateObjectSuccess_ValidObjectNoPramDefault()
+        {
+            var genericResult = new GenericResult();
+            Assert.True(genericResult.Success);
+        }
+        [Fact]
+        public void Contructor_CreateObjectNotNull_NoPramDefault()
+        {
+            var genericResult = new GenericResult();
+            Assert.Equal(expected: "test", actual: genericResult.Message);
+        }
+    }
+}
